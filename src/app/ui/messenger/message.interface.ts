2d5922a89ad08e9123ab8604a883ce2f47b70661
aacebo <aacebowork@gmail.com>
2019-12-02 21:49:32 -0500

    stuff

--- src/app/ui/messenger/message.interface.ts
+++ src/app/ui/messenger/message.interface.ts
@@ -2,6 +2,7 @@ import { MessageType } from './message-type.enum';
 
 export interface IMessage {
   readonly _id: string;
+  readonly _rev?: string;
   readonly type: MessageType;
   readonly content: string;
   readonly event?: string;
